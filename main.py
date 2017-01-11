35e4211de629c55c80169612d9db3c3cc4a4188c
Vladislav Smirnov <beluckydaf@gmail.com>
2017-01-11 18:18:24 +0500

    * Updated one comment

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import answers
 import constants
 
 bot = telebot.TeleBot(constants.token)
+schedule_queue = []
 
 print(bot.get_me())
 
@@ -27,7 +28,8 @@ def handle_start(message):
     user_markup.row('/start', '/about', '/help', '/stop')
     user_markup.row('Расписание', 'Как тебя зовут?')
     bot.send_message(message.from_user.id, 'Я рад, что ты к нам присоединился!\nМеню создано.', reply_markup=user_markup)
-    log(message, "Меню убрано.")
+    log(message, "Меню создано.")
+
 
 @bot.message_handler(commands=['stop'])
 def handle_stop(message):
@@ -51,8 +53,37 @@ def handle_about(message):
 # Then texts
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    answer = answers.cant_understand
-    if message.text.lower() == "как тебя зовут?":
+    if message.from_user.id in schedule_queue:
+        if message.text == "101":
+            bot.send_message(message.from_user.id, "ЗДЕСЬ БУДЕТ РАСПИСАНИЕ 101 ГРУППЫ!")
+            answer = "ЗДЕСЬ БУДЕТ РАСПИСАНИЕ 101 ГРУППЫ!"
+        elif message.text == "102":
+            bot.send_message(message.from_user.id, "ЗДЕСЬ БУДЕТ РАСПИСАНИЕ 102 ГРУППЫ!")
+            answer = "ЗДЕСЬ БУДЕТ РАСПИСАНИЕ 102 ГРУППЫ!"
+        elif message.text == "103":
+            bot.send_message(message.from_user.id, "ЗДЕСЬ БУДЕТ РАСПИСАНИЕ 103 ГРУППЫ!")
+            answer = "ЗДЕСЬ БУДЕТ РАСПИСАНИЕ 103 ГРУППЫ!"
+        elif message.text == "104":
+            bot.send_message(message.from_user.id, "ЗДЕСЬ БУДЕТ РАСПИСАНИЕ 104 ГРУППЫ!")
+            answer = "ЗДЕСЬ БУДЕТ РАСПИСАНИЕ 104 ГРУППЫ!"
+        elif message.text == "111":
+            bot.send_message(message.from_user.id, "ЗДЕСЬ БУДЕТ РАСПИСАНИЕ 111 ГРУППЫ!")
+            answer = "ЗДЕСЬ БУДЕТ РАСПИСАНИЕ 111 ГРУППЫ!"
+        elif message.text == "112":
+            bot.send_message(message.from_user.id, "ЗДЕСЬ БУДЕТ РАСПИСАНИЕ 112 ГРУППЫ!")
+            answer = "ЗДЕСЬ БУДЕТ РАСПИСАНИЕ 112 ГРУППЫ!"
+        elif message.text == "113":
+            bot.send_message(message.from_user.id, "ЗДЕСЬ БУДЕТ РАСПИСАНИЕ 113 ГРУППЫ!")
+            answer = "ЗДЕСЬ БУДЕТ РАСПИСАНИЕ 113 ГРУППЫ!"
+        elif message.text == "114":
+            bot.send_message(message.from_user.id, "ЗДЕСЬ БУДЕТ РАСПИСАНИЕ 114 ГРУППЫ!")
+            answer = "ЗДЕСЬ БУДЕТ РАСПИСАНИЕ 114 ГРУППЫ!"
+        else:
+            bot.send_message(message.from_user.id, "НЕТ ТАКОЙ ГРУППЫ!")
+            answer = "НЕТ ТАКОЙ ГРУППЫ!"
+        schedule_queue.remove(message.from_user.id)
+    # answer = answers.cant_understand WHY IS IT NOT NEEDED?
+    elif message.text.lower() == "как тебя зовут?":
         bot.send_message(message.from_user.id, answers.myname)
         answer = answers.myname
     elif message.text == "Стив":
@@ -60,8 +91,12 @@ def handle_text(message):
         answer = answers.steve
     # TODO: Расписание!
     elif message.text == "Расписание":
-        bot.send_message(message.from_user.id, answers.in_development)
-        answer = answers.in_development
+        schedule_queue.append(message.from_user.id)
+        user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
+        user_markup.row('101', '102', '103', '104')
+        user_markup.row('111', '112', '113', '114')
+        bot.send_message(message.from_user.id, answers.schedule, reply_markup=user_markup)
+        answer = answers.schedule + "\nДобавлен в лист ожидания ответа."
     else:
         bot.send_message(message.from_user.id, answers.cant_understand)
         answer = answers.cant_understand
